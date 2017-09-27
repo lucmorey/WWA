5ae4b3c0f54615098e5026aee4981baeef9e1941
Luc Morey <lucmorey@gmail.com>
2017-09-27 15:48:22 -0700

    more styles and CRUD working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    if @user.update(user_params)
+    if @user.update_attributes(user_params)
       redirect_to user_path(@user)
     else
       flash[:danger] = "error"
